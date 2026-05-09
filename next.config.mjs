a9a598da439551ae91ecbc16f27028092f8aca8b
tatsuya197102-blip <tatsuya197102@gmail.com>
2026-05-09 22:16:35 +0900

    fix: simplify next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Firebaseをサーバーコンポーネントから使う場合に必要
-  experimental: {
-    serverComponentsExternalPackages: ['firebase-admin'],
-  },
-}
-
+const nextConfig = {}
 export default nextConfig
